f160efb7bc2a14f26836c3b68024c82460cccb9b
Roku1234567 <s1f102401595@iniad.org>
2024-05-07 01:08:09 +0900

    マインスイーパー２

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,19 +24,31 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
-  };
+  const Playing = userInputs.some((row) => row.some((input) => input !== 0));
+  const Failure = userInputs.some((row, y) =>
+     row.some((input, x) => input === 1 && bombMap[y][x] === 1),
+
+   const borad: number[][] = [];
+   const newBoard = structuredClone(userInputs);
+
+   const clickHandler = (x: number, y: number) => {
+     console.log(x, y);
+
   const [samplePos, setSamplePos] = useState(0);
   console.log(samplePos);
   return (
     <div className={styles.container}>
-      <div className={styles.Boardstyle} />
-      <div
+      <div className={styles.boardStyle}>
+        {userInputs.map((row, y) =>
+          row.map((color, x) => <div className={styles.cellStyle} key={`${x}-{y}`} />),
+        )}
+
+        {/* <div
         className={styles.sampleStyle}
         style={{ backgroundPosition: `-${30 * samplePos}px 0px` }}
       />
-      <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button>
+      <button onClick={() => setSamplePos((p) => (p + 1) % 14)}>sample</button> */}
+      </div>
     </div>
   );
 };
