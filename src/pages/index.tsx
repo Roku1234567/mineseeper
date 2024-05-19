1b7bce2cc04f4985b277232d6a33b01a307197a5
Roku1234567 <s1f102401595@iniad.org>
2024-05-19 23:58:33 +0900

    ランダム

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [bombMap, setBombMap] = useState([
+  const [bombMap, setBombMap] = useState<(0 | 1 | 2 | 3)[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,23 +24,39 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const Playing = userInputs.some((row) => row.some((input) => input !== 0));
-  const Failure = userInputs.some((row, y) =>
-     row.some((input, x) => input === 1 && bombMap[y][x] === 1),
 
-   const borad: number[][] = [];
-   const newBoard = structuredClone(userInputs);
+  // const Playing = userInputs.some((row) => row.some((input) => input !== 0));
+  // const Failure = userInputs.some((row, y) =>
+  //   row.some((input, x) => input === 1 && bombMap[y][x] === 1),
+  // );
 
-   const clickHandler = (x: number, y: number) => {
-     console.log(x, y);
+  // const board: number[][] = [];
+  // const newBoard = structuredClone(board);
+  // newBoard[y][x] = bomb;
 
-  const [samplePos, setSamplePos] = useState(0);
-  console.log(samplePos);
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+    const newbombMap = structuredClone(bombMap);
+    let a = 0;
+    if (a === 0) {
+      for (let n = 0; n < 10; n++) {
+        const rx = Math.floor(Math.random() * 9);
+        const ry = Math.floor(Math.random() * 9);
+        console.log(rx, ry);
+        newbombMap[rx][ry] = 11;
+        a++;
+      }
+    }
+    setBombMap(newbombMap);
+  };
+
+  //   const [samplePos, setSamplePos] = useState(0);
+  //   console.log(samplePos);
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
         {userInputs.map((row, y) =>
-          row.map((color, x) => <div className={styles.cellStyle} key={`${x}-{y}`} />),
+          row.map((color, x) => <div className={styles.cellStyle} key={`${x}-${y}`} />),
         )}
 
         {/* <div
@@ -52,5 +68,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
